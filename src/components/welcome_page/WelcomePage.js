0ce9a803465da069b98835f31e983d4297eab60f
Edward Blanciak <blanciak.edward2@gmail.com>
2022-07-01 00:50:12 -0400

    Welcome page refactored and redesigned. Animation refactored in react-spring.

--- src/components/welcome_page/WelcomePage.js
+++ src/components/welcome_page/WelcomePage.js
@@ -1,12 +1,39 @@
-import Switch from '@mui/material/Switch';
-import { ThemeProvider } from 'styled-components';
-import { Wrapper } from '../../globalStyles';
+import { useSpring } from 'react-spring';
+import {
+	WelcomeWrapper,
+	Reflection,
+	TextWrapper,
+	Border,
+} from './WelcomePage.styles';
+import { FaDoorOpen } from 'react-icons/fa';
 
 const WelcomePage = () => {
+	const enterAnimation = useSpring({
+		// NOTE - left to right animation may look better
+		to: { transform: 'translate(0,15rem)' },
+		from: { transform: 'translate(0,-5.00rem) ' },
+		config: { duration: 1250 }, // 1500
+	});
+
+	const fadeIn = useSpring({
+		to: { opacity: 1 },
+		from: { opacity: 0 },
+		delay: 1000,
+		config: { duration: 3000 },
+	});
+
 	return (
-		<Wrapper>
-			<h1>Welcome</h1>
-		</Wrapper>
+		<WelcomeWrapper style={enterAnimation}>
+			<TextWrapper>w e l c o m e</TextWrapper>
+			<Reflection style={fadeIn}>w e l c o m e</Reflection>
+			{/* <Border /> */}
+			{/* <div>
+				<FaDoorOpen />
+			</div>
+			<Reflection>
+				<FaDoorOpen />
+			</Reflection> */}
+		</WelcomeWrapper>
 	);
 };
 
--- src/components/welcome_page/WelcomePage.styles.js
+++ src/components/welcome_page/WelcomePage.styles.js
@@ -1,1 +1,61 @@
 import styled from 'styled-components';
+import { animated } from 'react-spring';
+
+export const WelcomeWrapper = styled(animated.div)`
+	position: absolute;
+
+	font-size: 7rem;
+
+	@media (max-width: 1200px) {
+		font-size: 6rem;
+		left: 100px;
+	}
+
+	@media (max-width: 800px) {
+		font-size: 5rem;
+	}
+
+	@media (max-width: 600px) {
+		font-size: 4rem;
+	}
+
+	@media (max-width: 450px) {
+		font-size: 3.5rem;
+	}
+`;
+
+export const TextWrapper = styled.div``;
+
+export const Reflection = styled(animated.div)`
+	/* animation: fade-reflection 3s linear; */
+	transform: scaleY(-1);
+	-moz-transform: scaleY(-1);
+	-o-transform: scaleY(-1);
+	-webkit-transform: scaleY(-1);
+	transform: rotateX(180deg);
+	-moz-transform: rotateX(210deg);
+	-o-transform: rotateX(210deg);
+	-webkit-transform: rotateX(210deg);
+	perspective: 200px;
+	mask-image: -webkit-gradient(
+		linear,
+		right top,
+		right bottom,
+		from(transparent),
+		color-stop(20%, transparent),
+		to(rgba(0, 0, 0, 0.4))
+	);
+	-webkit-mask-image: -webkit-gradient(
+		linear,
+		right top,
+		right bottom,
+		from(transparent),
+		color-stop(20%, transparent),
+		to(rgba(0, 0, 0, 0.4))
+	);
+`;
+
+export const Border = styled(animated.div)`
+	border-top: 0.1rem solid white;
+	width: 25%;
+`;
